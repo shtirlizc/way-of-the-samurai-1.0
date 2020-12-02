1cee321abe4e0712d6d0f233b101aefc9df16f6e
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-02 13:49:33 +0500

    components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
-      <div>
-          <div>One div</div>
-          <div className="App">
-              <ul>
-                  <li>html</li>
-                  <li>css</li>
-                  <li>js</li>
-                  <li>react</li>
-              </ul>
-          </div>
-      </div>
-
+    <div>
+      <Header />
+      <Technologies />
+    </div>
   );
-}
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#1">Home</a>
+      <a href="#2">News Feed</a>
+      <a href="#3">Messages</a>
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+        <li>angular</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
