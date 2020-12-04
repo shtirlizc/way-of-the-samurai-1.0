8c1a7a2c6f5cc46d9052abe2eedfbd5851491918
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-04 12:08:44 +0500

    import and export

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import Header from "./Header";
+import Technologies from "./Technologies";
+
 import "./App.css";
 
 const App = () => {
@@ -5,30 +9,6 @@ const App = () => {
     <div>
       <Header />
       <Technologies />
-    </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#1">Home</a>
-      <a href="#2">News Feed</a>
-      <a href="#3">Messages</a>
-    </div>
-  );
-};
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-        <li>react</li>
-        <li>angular</li>
-      </ul>
     </div>
   );
 };
---
+++ src/Technologies.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+        <li>angular</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Technologies;
