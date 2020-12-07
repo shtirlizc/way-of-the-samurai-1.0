cc05b64ad20b2a88a765c22c05320e67c2664204
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-07 12:06:38 +0500

    start markup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from "react";
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className="app-wrapper">
+      <header className="app-header">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQMSnkj-RZcyHanSi6kGpgq-igu6HChcNtPg&usqp=CAU"
+          alt=""
+        />
+      </header>
+      <aside className="app-side">
+        <nav className="app-nav">
+          <a href="#1">Profile</a>
+          <a href="#1">Messages</a>
+          <a href="#1">News</a>
+          <a href="#1">Music</a>
+          <a href="#1">Settings</a>
+        </nav>
+      </aside>
+      <main className="app-content">
+        <div className="main-image">
+          <img
+            src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+            alt=""
+          />
+        </div>
+        <div className="profile">
+          <div className="profile__avatar">
+            <img
+              src="https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg"
+              alt=""
+            />
+          </div>
+          <div className="profile__desc"></div>
+        </div>
+        <div className="posts">
+          <div className="posts__new">New</div>
+          <div className="posts__feed">Feed</div>
+        </div>
+      </main>
     </div>
   );
 };
--- src/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return <div>it-kamasutra footer</div>;
-};
-
-export default Footer;
