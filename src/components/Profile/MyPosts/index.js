94d0379ab3b3b78a0ec856351ee0f82f31756d69
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-18 09:46:39 +0500

    posts array to top component

--- src/components/Profile/MyPosts/index.js
+++ src/components/Profile/MyPosts/index.js
@@ -3,20 +3,8 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post";
 
-const MyPosts = () => {
-  const posts = [
-    {
-      id: 1,
-      message: "Hi, how are you?",
-      likesCount: 12,
-    },
-    {
-      id: 2,
-      message: "It's, my first post",
-      likesCount: 11,
-    },
-  ];
-
+const MyPosts = (props) => {
+  const { posts } = props;
   const postsElements = posts.map(({ id, message, likesCount }) => (
     <Post key={{ id }} message={message} likes={likesCount} />
   ));
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,11 +2,34 @@ import React from "react";
 import MyPosts from "./MyPosts";
 import ProfileInfo from "./ProfileInfo";
 
+const posts = [
+  {
+    id: 1,
+    message: "Hi, how are you?",
+    likesCount: 12,
+  },
+  {
+    id: 2,
+    message: "Yo yo yo",
+    likesCount: 1024,
+  },
+  {
+    id: 3,
+    message: "Yo",
+    likesCount: 111,
+  },
+  {
+    id: 4,
+    message: "It's, my first post",
+    likesCount: 11,
+  },
+];
+
 const Index = () => {
   return (
     <>
       <ProfileInfo />
-      <MyPosts />
+      <MyPosts posts={posts} />
     </>
   );
 };
