9d315654730f87477d5574320648da92c69ed315
Marat Sarvarov <shtirlizc@gmail.com>
2021-01-19 10:49:51 +0500

    ajax to componentDidMount

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -6,13 +6,14 @@ import DefaultUserImage from "../../assets/images/user.png";
 import s from "./Users.module.css";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        props.setUsers(response.data.items);
-      });
+  componentDidMount() {
+    if (!this.props.users.length) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
+    }
   }
 
   onFollow = (userId) => {
